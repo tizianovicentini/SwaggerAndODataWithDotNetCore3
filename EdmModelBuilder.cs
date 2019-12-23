4169afabfe1f741b967a0c454b61581452e2645d
Tiziano Vicentini <tiziano.vicentini@altromercato.it>
2019-12-23 14:28:30 +0100

    Edm models for OData

---
+++ EdmModelBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.OData.Edm;
+
+namespace NSwagAppProto01
+{
+    public static class EdmModelBuilder
+    {
+        private static IEdmModel _edmModel;
+
+        public static IEdmModel GetEdmModel()
+        {
+            if (_edmModel == null)
+            {
+                var builder = new ODataConventionModelBuilder();
+                builder.EntitySet<WeatherForecast>("WeatherForecasts");
+
+                _edmModel = builder.GetEdmModel();
+            }
+
+            return _edmModel;
+        }
+    }
+
+}
