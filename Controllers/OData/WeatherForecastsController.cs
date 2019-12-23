6c29c392db5abf138d6b5c1480af3e63732d837a
Tiziano Vicentini <tiziano.vicentini@altromercato.it>
2019-12-23 14:27:59 +0100

    OData specific controller with Get API

---
+++ Controllers/OData/WeatherForecastsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NSwagAppProto01.Controllers.OData
+{
+    /// <summary>
+    /// OData endpoint: Weather Forecast
+    /// </summary>
+    public class WeatherForecastsController : ODataController
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastsController> _logger;
+
+        public WeatherForecastsController(ILogger<WeatherForecastsController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get Weather Forecasts
+        /// </summary>
+        /// <returns>
+        /// Some forecasts
+        /// </returns>
+        [EnableQuery]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+    }
+}
